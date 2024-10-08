123ff2d3cae0109a311d83de9f2f491d78e06007
Gauri2105 <gujargauri00@gmail.com>
2024-10-08 16:54:36 +0530

    files uploaded

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Modern Business - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+<?php include_once('includes/header.php');?>
+            <!-- Header-->
+            <!-- About section one-->
+            <section class="py-5 bg-light" id="scroll-target">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="images/about-us.jpg" alt="..." /></div>
+                        <div class="col-lg-6">
+                            <h2 class="fw-bolder">About Us</h2>
+                            <p class="lead fw-normal text-muted mb-0">Teachers form a main part of any institution that concerns with.
+The Teacher Record Management System contains data and information about the teacher.
+The main purpose of TRMS is to systematically record, store and update the teacher's records.
+Progressions can be brought about in the overall system of education, when the concept of teacher management is identified in an appropriate manner.
+Teachers are the ones who impart knowledge and information to the students in such a manner and ensure that their growth and development takes place in an operative manner and they are able to achieve their academic goals and objectives.
+The information from TRMS is used to search teachers online. With the help of this software, a person can easily search for a teacher according to his/her requirement.
+</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+   
+        </main>
+        <!-- Footer-->
+<?php include_once('includes/footer.php');?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
